f00eb3a536685d85b3e252d76d64904aa5b1dd13
Rajat6697 <rajatverma5338@gmail.com>
2023-01-11 00:10:31 +0530

    Header Item added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import {SearchIcon, ShoppingBagIcon, UserIcon} from '@heroicons/react/outline'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4'>
+      <div className='flex items-center justify-center md:w-1/5'>
+      <Link href="/">
+      <div className="relative h-10 w-5 cursor-pointer opacity-75 transition hover:opacity-100">
+        <Image alt='apple-logo' src={"https://rb.gy/vsvv2o"} width="100" height="100" />
+      </div>
+      </Link>
+      </div>
+      <div className='hidden flex-1 items-center justify-center space-x-8 md:flex'>
+        <a className='headerLink'>Product</a>
+        <a className='headerLink'>Explore</a>
+        <a className='headerLink'>Support</a>
+        <a className='headerLink'>Business</a>
+      </div>
+      <div className=''>
+        <SearchIcon className='headerIcon'/>
+      </div>
+    </header>
   )
 }
 
