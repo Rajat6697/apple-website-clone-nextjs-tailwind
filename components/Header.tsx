7423dd2b3f08f694bedb2ff8daf65eb326e4c509
Rajat6697 <rajatverma5338@gmail.com>
2023-01-28 03:04:05 +0530

    Added landing page component and minor changes in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import {SearchIcon, ShoppingBagIcon, UserIcon} from '@heroicons/react/outline'
+import {SearchIcon, ShoppingBagIcon, UserIcon} from '@heroicons/react/outline';
+import manAvatarImg from '../public/man-avatar.png';
 
 const Header = () => {
+
+  const session = false;
+
   return (
     <header className='sticky top-0 z-30 flex w-full items-center justify-between bg-[#E7ECEE] p-4'>
       <div className='flex items-center justify-center md:w-1/5'>
@@ -19,9 +23,35 @@ const Header = () => {
         <a className='headerLink'>Support</a>
         <a className='headerLink'>Business</a>
       </div>
-      <div className=''>
+      <div className='flex items-center justify-center gap-x-4 md:w-1/4'>
         <SearchIcon className='headerIcon'/>
+
+     <Link href="/checkout">
+     <div className='relative cursor-pointer'>
+        <span className='absolute -right-1 -top-1 z-50 h-4 w-4 flex items-center justify-center rounded-full bg-gradient-to-r from-pink-500 to-violet-500 text-[10px] text-white'>
+          6
+        </span>
+        <ShoppingBagIcon className='headerIcon' />
       </div>
+     </Link>
+
+    {
+      !session ? 
+      <Image
+      src={manAvatarImg}
+      alt = "man-avatar"
+      width="34"
+      height="34"
+      className='headerIcon'
+      // onClick={signOut}
+      /> :
+      <UserIcon className='headerIcon' 
+      // onClick={signIn}
+      />
+    }
+
+     </div>
+
     </header>
   )
 }
